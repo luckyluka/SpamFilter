81c7f2555c3266d053ec6138234a0cd6f7d27f0f
luka <luka.kavcic@it-elektronika.com>
2017-09-10 20:42:21 +0200

    update

--- spam_f.py
+++ spam_f.py
@@ -10,14 +10,11 @@ import math
 from heapq import nlargest
 import nltk
 import re
-#from nltk.stem.lancaster import LancasterStemmer
+import time 
 
-#from nltk.tokenize import RegexpTokenizer
-
-#toker = RegexpTokenizer(r'((?<=[^\w\s])\w(?=[^\w\s])|(\W))+', gaps=True)
+start_time = time.time()
 
 
-filet = open("word_test.txt","a")
 
 all_word_probs = 1
 joint_prob = 1
@@ -53,13 +50,18 @@ predval = 0
 actval = 0
 countmiss = 0
 counthit = 0
-
+undefined = 0
+hitpositive = 0
+hitnegative = 0
+falsepositive = 0
+falsenegative = 0
 
 conn = sqlite3.connect("test.db")
 c = conn.cursor()
+count_words = 0
 
 
-print "DB opened successfully"
+
 
 c.execute('''CREATE TABLE IF NOT EXISTS DICTIONARY
        (ID INT PRIMARY KEY  NOT NULL,
@@ -70,16 +72,14 @@ c.execute('''CREATE TABLE IF NOT EXISTS DICTIONARY
         WM              INT NOT NULL,
         WIS             INT NOT NULL, 
         WP              INT NOT NULL,
-        MI              INT NOT NULL);''')
-print "Table created successfully";
+        MI              INT NOT NULL, 
+        WIH             INT NOT NULL);''')
 
-#conn.text_factory = str
-emails = [os.path.join("/home/luka/Documents/data2/denemovse/metodologija/all_data/ready",f) for f in os.listdir("/home/luka/Documents/data2/denemovse/metodologija/all_data/ready")]    
+emails = [os.path.join("/home/luka/Documents/FAKS/metodologija/all_data/ready",f) for f in os.listdir("/home/luka/Documents/FAKS/metodologija/all_data/ready")]    
 
 
 
 for mail in emails: 
-    print mail
     
     mailcheck = mail[-5:-4]
     
@@ -87,11 +87,9 @@ for mail in emails:
     
     
     if mailcheck == "H":
-        print "this mail is HAM"
         number_ham_mails = number_ham_mails + 1
         actval = 0
     elif mailcheck == "S":
-        print "this mail is SPAM"
         number_spam_mails = number_spam_mails + 1
         actval = 1
     else:
@@ -105,104 +103,60 @@ for mail in emails:
             words = line.split() 
             for word in words:
 
-                print "MAIL NUMBER#: ", number_mails
 
                 ############ INCREMENT WORD COUNTER #####################
                 words_in_mail = words_in_mail + 1
                 #########################################################
                 word = word.decode("utf-8")
                 word = word.lower()
-                print word
-                #word = toker.tokenize(word)
-                #word = word.translate(None, string.punctuation)
-
-
+             
             
                 word = re.sub(r'[^\w\s]','',word)
-
-                
-                #cleanr = re.compile('<.*?>')
-                #word = re.sub(cleanr, '', word)
-                
                 ########### DELIMITER CONDITIONS ########################
                 if word == ">" or word =="|" or word =="{" or word == "}" or word == ">>" or word == "[" or word == "]" \
                 or word == "#" or word == "*" or word == "-" or word == "<a" or word == "/>" or word == "</tr>"\
                 or word == "<td" or word == "--" or word == "<p" or word == "<TR>" or word =="<TD" or word == "<tr>" or word == "=":
                     continue
                     
-                print "WORD: ", word
 
                 ##### CHECK IF WORD EXIST AND INSERT IF NOT EXISTS############
                 count = c.execute("SELECT count(*) FROM DICTIONARY WHERE WORD = ?", (word,))
                 for row in count:
                     test = row[0]
- 
-                #print "TEST: ", test   
-                if test == 0:
-                    print "NEW WORD"
-                    ################## START WITH ONE FOR ZERO DIVISION REASONS ############
                     
-
+                
+                if test == 0:
+                    
                     if actval == 0:
-                        print "ACTVAL:0"
-                        ns = 0
-                        nl = 1
-                        n = 1
-                        c.execute("INSERT OR IGNORE INTO DICTIONARY (ID,WORD,N,NS,NL, WM, WIS, WP, MI) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)", (idn, word, n, ns, nl, 1, 0, 0, 0));
-                    else:
-                        print "ACTVAL:1"
                         ns = 1
-                        nl = 0
-                        n = 1
-                        c.execute("INSERT OR IGNORE INTO DICTIONARY (ID,WORD,N,NS,NL, WM, WIS, WP, MI) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)", (idn, word, n, ns, nl, 1, 0, 0, 0));
+                        nl = 2
+                        n = 2
+                        c.execute("INSERT OR IGNORE INTO DICTIONARY (ID,WORD,N,NS,NL, WM, WIS, WP, MI, WIH) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", (idn, word, n, ns, nl, 1, 0, 0, 0, 0));
+                    else:
+                        ns = 2
+                        nl = 1
+                        n = 2
+                        c.execute("INSERT OR IGNORE INTO DICTIONARY (ID,WORD,N,NS,NL, WM, WIS, WP, MI, WIH) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", (idn, word, n, ns, nl, 1, 0, 0, 0, 0));
                     conn.commit()
                     ### INDEX INCREMENT##
                     idn = idn + 1
 
                     spamprob = float(number_spam_mails / number_mails)  ### PROBABILITY OF MAIL BEING A SPAM MAIL
                     hamprob = float(number_ham_mails / number_mails)
-
-                    
                     wordinspamprob = float(ns / n) #### PROBABILITY OF WORD BEING IN A SPAM MAIL
-
                     wordinhamprob = float(nl / n) #### PROBABILITY OF WORD BEING IN A SPAM MAIL
-                    
-                    #filet.write("%s, %s, %s, %s,   INSERT\n" %(word, ns, number_mails, wordinspamprob))
-                    
-                    wordprob = wordinspamprob * spamprob + wordinhamprob * hamprob  ##### PROBABILITY OF WORD APPEARING IN A MAIL
-                 
-                    print mail
-                    
-
                     ###################### UPDATE PROB DATA ########################################
-
-
-                    c.execute("UPDATE DICTIONARY SET WIS = ? WHERE WORD=?", (wordinspamprob, word))
-                    c.execute("UPDATE DICTIONARY SET WP = ? WHERE WORD=?", (wordprob, word))
-                
-
-                    conn.commit()
-
-               
-
-
+                    c.execute("SELECT COUNT (*) FROM DICTIONARY")
+                    nuber_of_words_in_dictionary = c.fetchone()[0]
                     ####################### MI INDEX CALC ######################
-                    print wordprob
-                    print spamprob
-                    print wordinspamprob
                     try:
-                        mi = math.log((wordinspamprob)/(wordprob*spamprob))
+                        mi = math.log((wordinspamprob)/((2 / nuber_of_words_in_dictionary)*spamprob))
                     except Exception as e:
                         mi = 0
-                    
-
-                    c.execute("UPDATE DICTIONARY SET MI = ? WHERE WORD=?", (mi, word))
-                    
+                    c.execute("UPDATE DICTIONARY SET WIS = ?, WIH = ?, MI = ? WHERE WORD=?", (wordinspamprob, wordinhamprob, mi, word))
                     conn.commit()
                 else:
-                    print "KNOWN WORD"
                     pass
-                    #print "known word-----------------------------"
                 ################### mark that word is in message ###############################
                 
 
@@ -211,18 +165,13 @@ for mail in emails:
                 for data in get_data:
                     foc = data[0]
 
-        
                 #######################################################################################
-
-
-
                 ########## UPDATE STATS FOR WORD ############################### do not increment if word already appeared in mail
                 if foc == 0:
-                    print "WORD PRESENT IN CURRENT MAIL FOR THE FIRST TIME"
-                    c.execute("UPDATE DICTIONARY SET WM = ? WHERE WORD=?", (1, word))
+                    #c.execute("UPDATE DICTIONARY SET WM = ? WHERE WORD=?", (1, word))
                     conn.commit()
                     #### GET STATS FOR WORD################################################################
-                    get_data  = c.execute("SELECT N, NS, NL, COUNT(N) FROM DICTIONARY WHERE WORD = ?", (word,))
+                    get_data  = c.execute("SELECT N, NS, NL FROM DICTIONARY WHERE WORD = ?", (word,))
                     
                     word_frequency = 0
                     spam_word_frequency = 0
@@ -233,193 +182,185 @@ for mail in emails:
                         word_frequency = data[0]
                         spam_word_frequency = data[1]
                         ham_word_frequency = data[2]
-                        word_count = data[3]
-
-                    print word_frequency
-                    print spam_word_frequency
-                    print ham_word_frequency
+                        
 
                     ns = spam_word_frequency
                     nl = ham_word_frequency
-
                     n = word_frequency + 1
-                    c.execute("UPDATE DICTIONARY SET N = ? WHERE WORD=?", (n, word))
-                    
+
+
                     if mailcheck == "S":
                         
                         ns = ns + 1
-                        c.execute("UPDATE DICTIONARY SET NS = ? WHERE WORD=?", (ns, word)) 
-                        print "spam"
+                        
                     
                     elif mailcheck == "H":
                         
                         nl = nl + 1
-                        c.execute("UPDATE DICTIONARY SET NL = ? WHERE WORD=?", (nl, word))
-                        print "ham"
                     else:
                         pass
-                    ################################################################
-                    conn.commit()
 
 
                     spamprob = float(number_spam_mails / number_mails)  ### PROBABILITY OF MAIL BEING A SPAM MAIL
                     hamprob = float(number_ham_mails / number_mails)
-                    print "ns:", ns
-                    print "number mails:", number_mails
+
                     wordinspamprob = float(ns / n) #### PROBABILITY OF WORD BEING IN A SPAM MAIL
-                    
-                    wordinhamprob = float(nl / n) #### PROBABILITY OF WORD BEING IN A SPAM MAIL
-
-                    #filet.write("%s, %s, %s, %s,   UPDATE\n" %(word, ns, number_mails, wordinspamprob))
-                    
-                    wordprob = wordinspamprob * spamprob + wordinhamprob * hamprob   ##### PROBABILITY OF WORD APPEARING IN A MAIL
-                    
-                    print word_frequency
-                    print spam_word_frequency
-                    print ham_word_frequency
-
-                    
-
-                    ###################### UPDATE PROB DATA ########################################
+                    wordinhamprob = float(nl / n) #### PROBABILITY OF WORD BEING IN A SPAM MAIL 
 
 
-                    c.execute("UPDATE DICTIONARY SET WIS = ? WHERE WORD=?", (wordinspamprob, word))
-                    c.execute("UPDATE DICTIONARY SET WP = ? WHERE WORD=?", (wordprob, word))
-                
-
-                    conn.commit()
-
-               
-
-
+                    c.execute("SELECT COUNT (*) FROM DICTIONARY")
+                    number_of_words_in_dictionary = c.fetchone()[0]
                     ####################### MI INDEX CALC ######################
-                    print wordprob
-                    print spamprob
-                    print wordinspamprob
+                    
                     try:
-                        mi = math.log((wordinspamprob)/(wordprob*spamprob))
+                        mi = math.log((wordinspamprob)/((n/number_of_words_in_dictionary)*spamprob))
                     except Exception as e:
                         mi = 0
-                    
 
-                    c.execute("UPDATE DICTIONARY SET MI = ? WHERE WORD=?", (mi, word))
+                    c.execute("UPDATE DICTIONARY SET N = ?, NS = ?, NL = ?, WIS = ?, WIH = ?, MI = ?,  WM = ? WHERE WORD=?", (n, ns, nl, wordinspamprob, wordinhamprob, mi, 1,  word))
+                    ################################################################
                     conn.commit()
                 else:
-                    print "WORD ALREADY PRESENT IN MAIL"
-    #raw_input("ustav")                
+					pass               
     ################################################################
     ################### CALCULATE PROBS ###########################
-    print "###################################################################"
-    print "#################ACTUAL PROBABILITY CALCULATION ###################"
-    print "###################################################################"
-    get_data  = c.execute("SELECT WIS, WP, MI FROM DICTIONARY  WHERE WM = ? ORDER BY MI DESC LIMIT 60 ", (1,))        
+    ###################################################################"
+    #################ACTUAL PROBABILITY CALCULATION ###################"
+    ###################################################################"
+    get_data  = c.execute("SELECT WIS, WIH FROM DICTIONARY  WHERE WM = ? ORDER BY MI DESC LIMIT 75 ", (1,))        
+    #get_data  = c.execute("SELECT WIS, WIH FROM DICTIONARY  WHERE WM = ?", (1,))        
     
-    all_word_in_spam_probs = 1
- 
+    all_words_in_spam_probs = 1
+    all_words_in_ham_probs = 1
 
-    mi_list_prob = []
-    mi_list_word_prob = []
+    mi_list_prob_spam = []
+    mi_list_prob_ham = []
 
     for data in get_data:            
-        all_word_in_spam_probs = data[0]
-        all_word_probs = data[1]
-        
-        #all_mis = data[2]
-        #print all_mis
+        all_words_in_spam_probs = data[0]
+        all_words_in_ham_probs = data[1]
 
-        if all_word_probs == 0:
-            all_word_probs = 1
-
-        if all_word_in_spam_probs == 0:
+        if all_words_in_spam_probs == 0:
             all_word_in_spam_probs = 1
-        
 
-        ##joint_prob = joint_prob * all_word_in_spam_probs
-        ##joint_word_prob = joint_word_prob * all_word_probs
-        #perhaps an array 
-        mi_list_prob.append(all_word_in_spam_probs)
-        mi_list_word_prob.append(all_word_probs)
-        
-        
-    print mi_list_prob
-    print "################"
-    print mi_list_word_prob
+        if all_words_in_ham_probs == 0:
+            all_words_in_ham_probs = 1
 
-    result_joint_prob = np.prod(np.array(mi_list_prob))
-    result_joint_word_prob = np.prod(np.array(mi_list_word_prob))
+        mi_list_prob_spam.append(all_words_in_spam_probs)
+        mi_list_prob_ham.append(all_words_in_ham_probs)
 
-    print "##############"
-    print result_joint_prob
-    print result_joint_word_prob
-    print "##############"
-    #print joint_prob
-    #print joint_word_prob
-    print "##############"
-    #if joint_word_prob == 0:
-    #    joint_word_prob = 1
+    #print "MI LIST_ham:", mi_list_prob_ham
+    #print "MI LIST_spam:", mi_list_prob_spam
+    result_joint_prob_spam = np.prod(np.array(mi_list_prob_spam))
+    result_joint_prob_ham = np.prod(np.array(mi_list_prob_ham))
     
-    print "##############"
-    print words_in_mail
-    print number_mails
-    print "##############"
+
+    try:
+    	total_prob = spamprob * result_joint_prob_spam / ((result_joint_prob_spam * spamprob) + (result_joint_prob_ham * hamprob))
+    	#total_prob = spamprob * result_joint_prob_spam  / hamprob * result_joint_prob_ham 
+    except Exception as e:
+    	print "DIVISION BY ZERO"
     
-    total_prob = (spamprob * result_joint_prob )/ result_joint_word_prob
-
-    #print "j_prob:",joint_prob
-    #print "J_w_prob:",joint_word_prob
-    print total_prob
-    #file = open("tot_prob.txt","a")
-    #file.write("%s, %s, %s, %s\n" %(total_prob, spamprob, result_joint_prob, result_joint_word_prob))
-
-    #inp = raw_input("PRESS ANY KEY TO CONTINUE")
-    #raw_input("PRESS ANY KEY TO CONTINUE")
-
-    if total_prob < 0.5:
-        predval = 0
-    elif total_prob > 0.5:
-        predval = 1
+    
 
 
+    
+    ######## TWO WAY ####################
 
-    file = open("eff.txt","a")
+     
+    if total_prob >= 0.5:
+    	predval = 1
+    else:
+    	predval = 0
+    
+    
 
+    ######## THREE_WAY ##################
+
+    #if total_prob > 0.10 and total_prob < 0.90:
+    #	predval = 2
+    #elif total_prob >= 0.90:
+    #	predval = 1
+    #elif total_prob <= 0.10: 
+    #	predval = 0
+
+    #print "ACTUAL VALUE: ", actval
+    #print "PRED VALUE: ", predval
+    #print "TOTAL PROB: ", total_prob
+    #print "SPAM PROB: ", spamprob
+    #print "HAM PROB: ", hamprob
+    #print "RESULT JOINT PROB SPAM: ", result_joint_prob_spam
+    #print "RESULT JOINT PROB HAM: ", result_joint_prob_ham
+
+
+    file1 = open("eff1.txt","a")
+    #file2 = open("eff2.txt","a")
+    #file3 = open("eff3.txt","a")
+    file4 = open("tot_p.txt", "a")
+
+    ### 
+    ### actval - 0  ham
+    ### actval - 1 spam
 
     if predval == actval:
+        counthit = counthit + 1 
+        if predval == 1:
+        	hitpositive = hitpositive + 1
+        else:
+        	hitnegative = hitnegative + 1
         print "HIT"
-        counthit = counthit + 1
-        
+
     else:
+    	countmiss = countmiss + 1
+    	if predval == 1:	
+        	falsenegative = falsenegative + 1
+        if predval == 0:
+        	falsepositive = falsepositive + 1
+        if predval == 2:
+        	undefined = undefined + 1
         print "MISS"
-        countmiss = countmiss + 1
-        
-
-    eff = float(counthit/number_mails)
-
-    file.write("%s\n" %eff)
-    print "predval:", predval
-    print "actval:", actval
-    print "counthit:", counthit
-    print "countmiss:", countmiss
-
+    #print raw_input("SPACE TO CONTINUE")
+    peff = float(counthit/number_mails)
+    neff = float(countmiss/number_mails)
+    ueff = float(undefined/number_mails)
     
-
-    print words_in_mail
-    ###### RESET WORDS IN MAIL COUNTER ##############
-    words_in_mail = 0
-    #################################################
-        
+    
+    file1.write("%s\n" %peff)
+    #file2.write("%s\n" %neff)
+    #file3.write("%s\n" %ueff)
+    file4.write("%s\n" %total_prob)
+       
     ######### INCREMENT MAIL COUNTER ##############
     number_mails = number_mails + 1
-    ###############################################
+    ################################################
 
 
 
 
     c.execute("UPDATE DICTIONARY SET WM = ? WHERE WM = ?", (0,1))        
     conn.commit()
-    #raw_input("ustav")
 
-        # add correctness check: whether filter was right or wrong
-    print "############################################################################"
+elapsed_time = time.time() - start_time
 
 print "HIT ACCURACY: ",  counthit/number_mails
+print "MISS ACCURACY: ",  countmiss/number_mails
+print "UNDECIDED ACCURACY: ",  undefined/number_mails
+print "##################"
+print "HIT POSITIVE: ", hitpositive/number_mails
+print "HIT NEGATIVE: ", hitnegative/number_mails
+print "FALSE POSITIVE: ", falsepositive/number_mails
+print "FALSE NEGATIVE: ", falsenegative/number_mails
+
+print "TIME ELAPSED: ", elapsed_time/60
+
+
+
+print "###########################"
+print "# HIT ACCURACY: ",  counthit
+print "# MISS ACCURACY: ",  countmiss
+print "# UNDECIDED ACCURACY: ",  undefined
+print "##################"
+print "# HIT POSITIVE: ", hitpositive
+print "# HIT NEGATIVE: ", hitnegative
+print "# FALSE POSITIVE: ", falsepositive
+print "# FALSE NEGATIVE: ", falsenegative
